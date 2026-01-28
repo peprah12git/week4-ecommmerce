000a374ebbdf334d8e23086ae8eeffe2ac701e7d
Emmanuel Peprah Mensah <e.mensah@amalitech.com>
2026-01-28 17:55:36 +0000

    adding the my sql JDBC connection

---
+++ src/main/java/config/DatabaseConnection.java
@@ -0,0 +1,63 @@
+package config;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/ecommerce_db";
+    private static final String USER = "root";
+    private static final String PASSWORD = "noah_1@23.Djanor"; //PASSWORD
+
+    private static DatabaseConnection instance;
+    private Connection connection;
+
+    private DatabaseConnection() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            this.connection = DriverManager.getConnection(URL, USER, PASSWORD);
+            System.out.println("✓ Database connected!");
+        } catch (ClassNotFoundException | SQLException e) {
+            System.err.println("✗ Connection failed: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    public static DatabaseConnection getInstance() {
+        if (instance == null || !isConnectionValid()) {
+            instance = new DatabaseConnection();
+        }
+        return instance;
+    }
+
+    private static boolean isConnectionValid() {
+        try {
+            return instance != null && instance.connection != null &&
+                    !instance.connection.isClosed() && instance.connection.isValid(2);
+        } catch (SQLException e) {
+            return false;
+        }
+    }
+
+    public Connection getConnection() {
+        try {
+            if (connection == null || connection.isClosed()) {
+                connection = DriverManager.getConnection(URL, USER, PASSWORD);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
+
+    public void closeConnection() {
+        try {
+            if (connection != null && !connection.isClosed()) {
+                connection.close();
+                System.out.println("✓ Database closed.");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
